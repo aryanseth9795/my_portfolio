61f79690fdfefd41c024bbf2225b118bc516b8a9
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2023-05-14 20:06:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route , Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
@@ -20,6 +20,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
+               <Route path="/" element={ <Navigate to="/" /> } />
         </Routes>
 
         <Footer />
