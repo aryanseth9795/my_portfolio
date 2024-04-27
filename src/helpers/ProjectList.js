96d5f05a91da8937cb5097910a9b8674f53406d2
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-04-27 13:48:05 +0530

    Updated 24-04-24

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,24 +1,44 @@
-
-import Proj2 from "../assets/proj2.jpg";
-import bg1 from "../assets/bg1.jpg";
-import aryan from "../assets/aryan.jpg"
+import lms from '../assets/lms.jpg'
+import weather from '../assets/weather.jpg'
+import portf from '../assets/portf.jpg'
+import restro from '../assets/restro.jpg'
+import todo from '../assets/todo.jpg'
 
 
 export const ProjectList = [
   {
+    name: "LMS-Library Management System",
+    image: lms,
+    skills: "REACT JS,NODE JS,MONGODB,JAVASCRIPT,CSS,EXPRESS JS",
+    github:"https://github.com/aryanseth9795/library-management-sytem-Frontend",
+    link:"https://lms-aryan.netlify.app/"
+  },
+  {
+    name: "WEATHER APP",
+    image: weather,
+    skills: "HTML,CSS,JAVASCRIPT,REACT JS",
+    github:"https://github.com/aryanseth9795/",
+    link:"https://aryanseth9795.github.io/weather"
+  },
+  {
+    name: "Restaurent APP",
+    image: restro,
+    skills: "HTML,CSS,JAVASCRIPT,REACT JS",
+    github:"https://github.com/aryanseth9795/",
+    link:"https://aryanseth9795.github.io/restro"
+  },
+  {
     name: "Portfolio",
-    image: aryan,
-    skills: "HTML,CSS,JAVASCRIPT",
+    image: portf,
+    skills: "HTML,CSS,JAVASCRIPT, REACT JS",
+    github:"https://github.com/aryanseth9795/my_portfolio",
+    link:"https://aryanseth9795.github.io/my_portfolio/"
   },
   {
     name: "To-Do List",
-    image: bg1,
+    image: todo,
     skills: "HTML,CSS,JAVASCRIPT",
+    github:"https://github.com/aryanseth9795/",
+    link:"https://aryanseth9795.github.io/To-do-List/"
   },
-  {
-    name:"E-commerce",
-    image: Proj2,
-    skills: "React Js ,Javascript"
-  }
-
 ];
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
+import { Link } from "react-router-dom";
 
 function ProjectDisplay() {
   const { id } = useParams();
@@ -10,11 +11,16 @@ function ProjectDisplay() {
   return (
     <div className="project">
       <h1> {project.name}</h1>
-      <img src={project.image} />
+      <img src={project.image} alt =""/>
       <p>
         <b>Skills:</b> {project.skills}
       </p>
+      <Link to ={project.link} >
+      Website Link
+      </Link>
+      <Link to ={project.github} >
       <GitHubIcon />
+      </Link>
     </div>
   );
 }
